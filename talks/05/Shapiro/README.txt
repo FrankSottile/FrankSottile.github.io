549c1f3ebca05b3b3fba14badc2f5c326e7ebf5f
Frank Sottile <sottile@math.tamu.edu>
2023-09-04 18:59:10 -0500

    more talks

---
+++ talks/05/Shapiro/README.txt
@@ -0,0 +1,8 @@
+For Chambery, I am skipping the three slides (numbers 16, 25, 
+and 26) involving discriminants.
+This requires editing number 15 and 24, as well as PartII.html,
+as the first two contain pointers to the omitted pages,
+and the last mentions Discriminants.
+The Old slides will be <name>.IHP.html
+
+-Frank
---
+++ talks/05/Shapiro/page/images.pl
@@ -0,0 +1,16 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate images original text with physical files.
+
+
+$key = q/{picture}(172,154)(-10,0)put(-2,-3){includegraphics[height=160pt]{imagesslashDis115,10){{v=1{}put(-10,33){{w=-1{}put(-10,115){{w=1{}{picture};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="267" HEIGHT="255" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{picture}(172,154)(-10,0)
+\put(-2,-3){\includegraphics[height=160pt]{imag...
+...1$} \put(115,10){$v=1$}
+\put(-10,33){$w=-1$} \put(-10,115){$w=1$}
+\end{picture}">|; 
+
+1;
+
