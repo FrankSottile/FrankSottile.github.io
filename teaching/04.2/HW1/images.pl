e32195d7525b3915f0970bc026469149f70b52e2
Frank Sottile <sottile@math.tamu.edu>
2023-09-04 14:29:47 -0500

    trying to finish teaching

---
+++ teaching/04.2/HW1/images.pl
@@ -0,0 +1,68 @@
+# LaTeX2HTML 2002 (1.62)
+# Associate images original text with physical files.
+
+
+$key = q/displaystylesum_{lambdasubsetBox_{k,l}}q^{|lambda|}=binom{k+l}{k;l}_q,,;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="198" HEIGHT="65" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$\displaystyle \sum_{\lambda\subset\Box_{k,l}} q^{\vert\lambda\vert} = \binom{k+l}{k;l}_q ,
+$">|; 
+
+$key = q/q;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$ q$">|; 
+
+$key = q/n;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$ n$">|; 
+
+$key = q/binom{k+l}{k;l}_q;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="52" HEIGHT="42" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$ \binom{k+l}{k;l}_q$">|; 
+
+$key = q/k;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ k$">|; 
+
+$key = q/(q^n-1)slash(q-1)=q^{n-1}+q^{n-2}+dotsb+q^2+q+1;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="403" HEIGHT="38" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$ (q^n-1)/(q-1)=q^{n-1}+q^{n-2}+\dotsb+q^2+q+1$">|; 
+
+$key = q/displaystylesum_{lambdasubsetBox_{k,l}}1=binom{k+l}{k;l},,;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="174" HEIGHT="65" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$\displaystyle \sum_{\lambda\subset\Box_{k,l}} 1 = \binom{k+l}{k;l} ,
+$">|; 
+
+$key = q/Box_{k,l};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ \Box_{k,l}$">|; 
+
+$key = q/(l^k);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="33" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ (l^k)$">|; 
+
+$key = q/l;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ l$">|; 
+
+1;
+
