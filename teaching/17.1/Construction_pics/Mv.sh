e58516dc63790d3e16a7df9c2b9f14c7d1f1bbc6
Frank Sottile <sottile@tamu.edu>
2023-09-19 14:29:59 -0500

    fewer teaching files

--- teaching/17.1/Construction_pics/Mv.sh
+++
@@ -1,6 +0,0 @@
-ls *.JPG | cut --delimiter=.  --fields=-1  |
-while read u
-do
- echo $u
- mv  $u.jpg  $u_sm.jpg
-done
--- teaching/17.1/Construction_pics/Photo_convert.sh
+++
@@ -1,6 +0,0 @@
-ls *.JPG | cut --delimiter=.  --fields=-1  |
-while read u
-do
- echo $u
- convert $u.JPG -resize 18% $u.sm.jpg
-done
