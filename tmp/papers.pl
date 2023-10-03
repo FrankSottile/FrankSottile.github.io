8ef16bac1a139542070932e4f1bba9f806ca7be7
Frank Sottile <sottile@tamu.edu>
2023-10-02 20:53:19 -0500

    more teaching

---
+++ tmp/papers.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+##############################################################
+# Perl code to make a .grml page for easy printing
+my $fileName = "print.html";
+my $tmpFile = "ZZZ.tmp";
+
+system("ls > ZZZ.tmp");
